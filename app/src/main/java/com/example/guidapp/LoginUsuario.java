70e00ed2feca3cc9504cddc409206f3b793f4946
Gustavo <gustavofernandezluz@gmail.com>
2019-10-21 22:28:11 -0200

    usando intent no login

--- app/src/main/java/com/example/guidapp/LoginUsuario.java
+++ app/src/main/java/com/example/guidapp/LoginUsuario.java
@@ -2,13 +2,29 @@ package com.example.guidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginUsuario extends AppCompatActivity {
+
+    private EditText loginText, senhaText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_usuario);
+        this.loginText = (EditText)findViewById(R.id.etNomeLogin);
+        this.senhaText = (EditText)findViewById(R.id.etSenhaLogin);
+    }
+
+    public void login(View v) {
+        Intent it = new Intent(this, UsuarioPerfil.class);
+        it.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        it.putExtra("nome", loginText.getText().toString());
+        it.putExtra("senha", senhaText.getText().toString());
+        startActivity(it);
     }
 }
