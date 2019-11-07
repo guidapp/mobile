2f0b91d4e7cab5c0af2476b0da50f040ab2a9092
gustavofl <gustavofernandezluz@gmail.com>
2019-11-07 12:50:40 -0300

    Intents basicas

--- app/src/main/java/com/example/guidapp/LoginUsuario.java
+++ app/src/main/java/com/example/guidapp/LoginUsuario.java
@@ -2,13 +2,31 @@ package com.example.guidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginUsuario extends AppCompatActivity {
+
+//    private EditText etEmailLogin = (EditText) findViewById(R.id.etEmailLogin);
+//    private EditText etSenhaLogin = (EditText) findViewById(R.id.etSenhaLogin);
+//    private Button btLogin = (Button) findViewById(R.id.btLogin);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_usuario);
+    }
+
+    public void login (View v) {
+        Intent intent = new Intent(this, Roteiro.class);
+        startActivity(intent);
+    }
+
+    public void cadastro (View v) {
+        Intent intent = new Intent(this, CadastroUsuario.class);
+        startActivity(intent);
     }
 }
